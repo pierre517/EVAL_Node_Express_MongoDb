20bd7f1c2ad335b8b6b4deec78c3889abd7c9445
pierre <pierrebilotto58@gmail.com>
2025-10-26 11:27:23 +0100

    api moto

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,12 +1,27 @@
+// ------------------voitures----------------------------//
 
 export async function getDataAllVoitures() {
-    const response = fetch("http://localhost:3000/voitures");
-    const voitures = (await response).json();
-    return voitures;
+  const response = fetch("http://localhost:3000/voitures");
+  const voitures = (await response).json();
+  return voitures;
 }
 
-export async function  getDataOneVoiture(id) {
-    const response = fetch(`http://localhost:3000/voitures/${id}`);
-    const voiture = (await response).json();
-    return voiture;
+export async function getDataOneVoiture(id) {
+  const response = fetch(`http://localhost:3000/voitures/${id}`);
+  const voiture = (await response).json();
+  return voiture;
+}
+
+// ---------------------------motos----------------------//
+
+export async function getDataAllMotos() {
+  const response = fetch("http://localhost:3000/voitures");
+  const motos = (await response).json();
+  return motos;
+}
+
+export async function getDataOneMoto(id) {
+  const response = fetch(`http://localhost:3000/motos/${id}`);
+  const moto = (await response).json();
+  return moto;
 }
